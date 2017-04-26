12238600c2f3fd8b669b569fcb548aa8158531f0
boaz <boaz.sade@unicorn-cybersec.com>
2017-04-26 14:29:28 +0300

    add examples

---
+++ examples/create_json.cpp
@@ -0,0 +1,69 @@
+#include "json_parser/json_ostream.h"
+#include "json_parser/json_writer.h"
+#include "json_parser/jsonfwrd.h"
+#include <vector>
+#include <string>
+
+#include <iostream>
+const char* empty = "";
+
+struct bar {
+    int tt = 12;
+    std::string baz;
+
+};
+
+json::ostream& operator ^ (json::ostream& js, const bar& b) {
+    using namespace json::literals;
+    js ^ "int_arg"_n ^ b.tt ^ "string_arg"_n ^ b.baz;
+    return js;
+}
+
+struct foo {
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    std::string s;
+    double* dp = nullptr;
+    const char* cp =empty; 
+    std::vector<bar> list;
+    std::list<int> simple_list;
+};
+
+std::ostream& operator << (std::ostream& os, const foo& f) {
+    return os <<"a = "<<f.a<<", b = "<<f.b<<", c = "<<f.c<<" s = '"<<f.s<<"'"<<" dp = "<<(f.dp ? *f.dp : 0.0);
+}
+
+json::ostream& operator ^ (json::ostream& js, const foo& f) {
+    using namespace json::literals;
+    js ^ "a"_n ^ f.a ^ "b"_n ^ f.b ^ "c"_n ^ f.c ^ "s"_n ^ f.s ^ "dp"_n ^ f.dp ^ "cp"_n ^ f.cp;
+    auto sl = js ^ "simle_list"_s;
+    sl ^ f.simple_list ^ json::_end;
+   auto l = js ^ "struct_array"_s;
+    l ^ f.list ^ json::_end; 
+    return js;
+}
+
+int main() {
+    using namespace json::literals;
+
+    foo f;
+    const bar b[] = {
+        {234, std::string("jjj")},
+        {987, std::string("iuywieru")}
+    };
+    f.list.insert(f.list.begin(), std::begin(b), std::end(b));
+    f.simple_list.push_back(1);
+    f.simple_list.push_back(17);
+
+    //json::ostream::proptree_type root;
+    //json::ostream parser(root);
+    json::output_stream root;
+    auto parser = root ^ json::open;
+    auto s = parser ^ "start"_s;
+    s ^ f ^ json::_end;
+    auto s2 = parser ^ "second"_s;
+    s2 ^ json::_end;
+    auto msg = root ^ json::str_cast;
+    std::cout<<"message:\n"<<msg<<std::endl;
+}
---
+++ examples/read_json.cpp
@@ -0,0 +1,88 @@
+#include "libs/json_parser/json_istream.h" 
+#include <boost/filesystem.hpp> 
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iterator>
+#include <algorithm>
+
+bool report_error(const std::string& err)
+{
+    std::cerr<<"error: "<<err<<std::endl;
+    return false;
+}
+
+struct message_header 
+{ 
+    using reply_list = std::vector<std::string>;
+
+    std::string field;
+    std::string solution;   
+    std::string name;       
+    std::string uuid;
+    reply_list  expected_replies;
+
+};
+
+std::ostream& operator << (std::ostream& os, const message_header& hdr)
+{
+    os<<"field: "<<hdr.field<<", solution: "<<hdr.solution<<", name: "<<hdr.name<<", uuid: "<<hdr.uuid;
+    if (!hdr.expected_replies.empty()) {
+        std::copy(std::begin(hdr.expected_replies), std::end(hdr.expected_replies), std::ostream_iterator<std::string>(os, ","));
+    }
+    return os;
+}
+
+json::istream& operator ^ (json::istream& os, message_header& hdr) 
+{ 
+    using namespace json::literals; 
+    os ^ "solution_type"_n ^ hdr.field ^ "solution_sub_type"_n ^ hdr.solution ^ "message_type"_n ^ hdr.name ^ "uuid"_n ^ hdr.uuid; 
+    auto er = os ^ json::_child(os, "reply_type"_n); 
+    er ^ json::start_arr ^ hdr.expected_replies ^ json::end_arr; 
+    return os; 
+}
+
+bool get_header(const std::string& input, message_header& output) 
+{
+    try { 
+        json::istream_root root; 
+        root ^ input; 
+        auto base_node = root ^ json::_root; 
+        // read the message "header" - note that you need to tell where the 
+        // sub node is starting - the name of it 
+        auto header = base_node ^ json::_child(base_node, json::_name("title")); 
+        header ^ output; return true; 
+    } catch (const std::runtime_error& err) { 
+        return report_error(err.what()); 
+    } catch (...) { 
+        return report_error("failed to parse message header"); 
+    }
+    return true;
+}
+
+int main(int argc, char** argv)
+{
+    if (argc != 2) {
+        std::cerr<<"usage: "<<argv[0]<<" <input json file\n";
+        return -1;
+    } else {
+        std::cout<<"reading json from "<<argv[1]<<std::endl;
+        std::ostringstream buffer;
+        std::ifstream input(argv[1]);
+        if (input) {
+            buffer << input.rdbuf();
+            message_header hdr;
+            if (get_header(buffer.str(), hdr)) {
+                std::cout<<"successfully read header "<<hdr<<" from input file"<<std::endl;
+            } else {
+                std::cerr<<"failed to successsfully read hader from input file\n";
+                return -3;
+            }
+        } else {
+            std::cerr<<"failed to open "<<argv[1]<<" for reading\n";
+            return -2;
+        }
+}
+
