a83a0e70b97cd3d17d892c259a356954a3bd6cd4
Boaz Sade <boaz1sade@gmail.com>
2023-12-21 11:54:50 +0200

    Chore: adding support to build with cmake and docker

---
+++ build-debug.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT=$(realpath "$0")
+SOURCES=$(dirname "${SCRIPT}")
+
+. ${SOURCES}/.build_settings.sh
+
+BUILD_DIR_DOCKER=$PWD
+echo "building debug version"
+docker exec  ${IMAGE} ${BUILD_DIR_DOCKER}/build.sh -b ${DEBUG_BUILD_DIR} -t Debug
+
